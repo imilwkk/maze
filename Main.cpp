df7dd5f23ee6f2e8c802160bac1b0d879f28400e
imilwkk <milena.subanova@alatoo.edu.kg>
2025-04-14 00:25:16 +0600

    added : user input for size

--- Main.cpp
+++ Main.cpp
@@ -16,6 +16,13 @@ int dx[] = {-2, 0, 2, 0};
 int dy[] = {0, 2, 0, -2};
 
 int main() {
+    // Get maze size from user with validation
+    void getMazeSize() {
+        do {
+            cout << "Type the size of the maze (within 10 to 20, odd only): ";
+            cin >> n;
+        } while (n < 10 || n > 20 || n % 2 == 0);
+    }
 
     return 0;
 }
