6807545c3e86ba0379402e25d1dd9afc53ed0caa
imilwkk <milena.subanova@alatoo.edu.kg>
2025-04-14 00:22:02 +0600

    added : iomanip maze structure & basic setup

--- Main.cpp
+++ Main.cpp
@@ -2,3 +2,20 @@
 #include <vector>
 #include <cstdlib>
 #include <ctime>
+#include <iomanip>
+
+using namespace std;
+
+const int MAX_SIZE = 21;
+char maze[MAX_SIZE][MAX_SIZE];
+bool visited[MAX_SIZE][MAX_SIZE];
+
+int n;
+
+int dx[] = {-2, 0, 2, 0};
+int dy[] = {0, 2, 0, -2};
+
+int main() {
+
+    return 0;
+}
